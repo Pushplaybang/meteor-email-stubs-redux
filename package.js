0baf021d853e12c9a3a9fc888605c8ccaeee6d0c
Paul van Zyl <paul@nonacreative.com>
2017-03-12 16:32:59 +0200

    published.

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'pushplaybang:email-stub-redux',
-  version: '0.2.0',
+  version: '0.3.0',
   summary: 'Allows you to inspect sent emails and assert on their content.',
-  git: 'https://github.com/xolvio/meteor-email-stub',
+  git: 'https://github.com/Pushplaybang/meteor-email-stubs-redux',
   documentation: 'README.md',
   testOnly: true
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0.3.1');
-  api.use('ecmascript');
+  api.use('ecmascript@0.6.1');
   api.use(['email'], 'server');
   api.addFiles('email.js', 'server');
 });
